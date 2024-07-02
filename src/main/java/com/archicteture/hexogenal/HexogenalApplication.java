2b21f0655e89ff047d79d89aeeb8aa1d8b7b288b
Nikola Rakic <nikolarakic15@gmail.com>
2024-07-02 19:21:58 +0200

    init

---
+++ src/main/java/com/archicteture/hexogenal/HexogenalApplication.java
@@ -0,0 +1,13 @@
+package com.archicteture.hexogenal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexogenalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexogenalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/archicteture/hexogenal/HexogenalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archicteture.hexogenal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexogenalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
